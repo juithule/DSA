30a8f2feb2db274982cfcd6658bbd917c50f6c3e
Jui Thule <juithule8@gmail.com>
2024-10-28 22:13:50 +0530

    binary search added

---
+++ BinarySearch.java
@@ -0,0 +1,42 @@
+package src;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60};
+        int target = 30;
+
+        Arrays.sort(arr);
+
+        int result = binarySearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
