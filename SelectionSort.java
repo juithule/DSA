231051f5a447043ff9736d57833ceeb4ed8bfd42
Jui Thule <juithule8@gmail.com>
2024-10-28 22:22:37 +0530

    selection sort added

---
+++ SelectionSort.java
@@ -0,0 +1,36 @@
+package src;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int[] arr = {64, 25, 12, 22, 11};
+	        selectionSort(arr);
+	        System.out.println("Sorted array:");
+	        printArray(arr);
+	    }
+
+	    public static void selectionSort(int[] arr) {
+	        int n = arr.length;
+	        for (int i = 0; i < n - 1; i++) {
+	            int minIndex = i;
+	            for (int j = i + 1; j < n; j++) {
+	                if (arr[j] < arr[minIndex]) {
+	                    minIndex = j;
+	                }
+	            }
+	            int temp = arr[minIndex];
+	            arr[minIndex] = arr[i];
+	            arr[i] = temp;
+	        }
+	    }
+
+	    public static void printArray(int[] arr) {
+	        for (int value : arr) {
+	            System.out.print(value + " ");
+	        }
+	        System.out.println();
+	    }
+	}
+
+
